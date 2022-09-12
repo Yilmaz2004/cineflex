a4f87152bbeb01621e00f7f53d6a1d83b21a9726
Ahmed Omar <00286504@rijnijssel.nl>
2022-09-12 15:49:04 +0200

    baba

--- includes/workersview.inc.php
+++ includes/workersview.inc.php
@@ -2,7 +2,11 @@
 include '../private/conn.php';
 
 
-
+$sql = "SELECT firstname
+        FROM user
+        WHERE role = 'worker'";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
 
 
 ?>
@@ -10,9 +14,22 @@ include '../private/conn.php';
 
 
 <li class="table-header">
-    <div class="col col-1">Afbeelding</div>
-    <div class="col col-1">Naam</div>
-    <div class="col col-1">Datum</div>
-    <div class="col col-1">Beschrijving</div>
-    <div class="col col-1">Betaling toevoegen</div>
+    <div class="col col-1">Name</div>
 </li>
+
+
+
+<?php if ($stmt->rowCount() > 0) { ?><?php
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+
+?>
+
+<li class="table-header">
+    <div class="col col-1" data-label="Job Id"><?= $row["firstname"] ?></div>
+    <div class="col col-1" data-label="Job Id">
+    </div>
+</li>
+
+<?php }}?>
