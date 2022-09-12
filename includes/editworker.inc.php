a2d247a95fb32cc8eac0a73b125ef8c56d49108b
Ajuub Osman <00303637@rijnijssel.nl>
2022-09-12 12:22:09 +0200

    newest

---
+++ includes/editworker.inc.php
@@ -0,0 +1,31 @@
+<head>
+    <title>My Buddy</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+<div class="container mt-3">
+    <h2>Edit A Supervisor</h2>
+    <form action="php/editworker.php" method="POST" enctype="multipart/form-data">
+        <div class="mb-3 mt-3">
+            <label>Name:</label>
+            <input type="text" class="form-control" placeholder="Enter name" name="name">
+        </div>
+        <div class="mb-3 mt-3">
+            <label>Email:</label>
+            <input type="text" class="form-control" placeholder="Enter description" name="description">
+        </div>
+
+        <div class="mb-3 mt-3">
+            <label>Password:</label>
+            <input type="text" class="form-control" placeholder="Enter Password" name="Password">
+        </div>
+        <button name="submit" type="submit" class="btn btn-success">Add</button>
+    </form>
+</div>
+</body>
---
+++ includes/login.inc.php
@@ -0,0 +1,21 @@
+<head>
+    <title>CineFlex</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-3">
+    <h2>Login</h2>
+    <form action="#" method="POST" enctype="multipart/form-data">
+        <div class="mb-3 mt-3">
+            <label>Email:</label>
+            <input type="Email" class="form-control" placeholder="Enter Email" name="Email">
+        </div>
+        <div class="mb-3 mt-3">
+            <label>Description:</label>
+            <input type="Password" class="form-control" placeholder="Enter Password" name="Password">
+        </div>
+        <button name="submit" type="submit" class="btn btn-success">Login</button>
+    </form>
+</div>
+</body>
