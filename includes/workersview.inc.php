a11ba2aace0e2a2b00cdc1488ff1db8f4fae66d0
Ahmed Omar <00286504@rijnijssel.nl>
2022-09-12 16:15:13 +0200

    baba

--- includes/workersview.inc.php
+++ includes/workersview.inc.php
@@ -2,34 +2,55 @@
 include '../private/conn.php';
 
 
-$sql = "SELECT firstname
+$sql = "SELECT firstname,lastname
         FROM user
         WHERE role = 'worker'";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 
 
+if ($stmt->rowCount() > 0) {
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+        ?>
+
+
+
+    <table class="table">
+        <thead>
+        <tr>
+
+            <th scope="col">First name</th>
+            <th scope="col">Last name</th>
+            <th scope="col">Add</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+
+            <td><?= $row["firstname"] ?></td>
+            <td><?= $row["lastname"] ?></td>
+            <td>x</td>
+            <td>x</td>
+            <td><button type="button" class="btn btn-danger" onclick=" if(confirm('Weet u zeker dat u deze betaling wilt verwijderen?'))window.location.href='php/deleteworker.php?workerid=<?= $row["workerid"] ?>'"Danger</button></td>
+
+        </tr>
+
+
+        </tr>
+        </tbody>
+    </table>
+
+
+
+<?php
+
+    }}
 ?>
 
 
 
-<li class="table-header">
-    <div class="col col-1">Name</div>
-</li>
-
-
-
-<?php if ($stmt->rowCount() > 0) { ?><?php
-
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-
-
-?>
-
-<li class="table-header">
-    <div class="col col-1" data-label="Job Id"><?= $row["firstname"] ?></div>
-    <div class="col col-1" data-label="Job Id">
-    </div>
-</li>
-
-<?php }}?>
