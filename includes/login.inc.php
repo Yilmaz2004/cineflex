e21cc74790a7615999dc9e9c1afece8ccc2404f0
yilma <00312238@rijnijssel.nl>
2022-09-12 11:40:16 +0200

    ok

---
+++ includes/login.inc.php
@@ -0,0 +1,26 @@
+<head>
+    <title>My Buddy</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+<div class="container mt-3">
+    <h2><Login></Login></h2>
+    <form action="php/addgroup.php" method="POST" enctype="multipart/form-data">
+        <div class="mb-3 mt-3">
+            <label>Name:</label>
+            <input type="text" class="form-control" placeholder="Enter name" name="name">
+        </div>
+        <div class="mb-3 mt-3">
+            <label>Description:</label>
+            <input type="text" class="form-control" placeholder="Enter description" name="description">
+        </div>
+        <button name="submit" type="submit" class="btn btn-success">Login</button>
+    </form>
+</div>
+</body>
