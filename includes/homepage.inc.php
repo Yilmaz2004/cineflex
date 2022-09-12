2d1bec04a369b7bffdc07f4b031c2ff9db8f6e5f
Ajuub Osman <00303637@rijnijssel.nl>
2022-09-12 16:22:35 +0200

    newest

---
+++ includes/homepage.inc.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>cineflex</title>
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="header">
+    <h1>Cineflex</h1>
+</div>
+
+
+
+<div class="row">
+    <div class="leftcolumn">
+        <div class="card">
+            <h2>Gedragsnormen</h2>
+            <div class="fakeimg" style="height:200px;">Image</div>
+            <p>Om al onze gasten nu en in de toekomst zo comfortabel mogelijk te kunnen laten genieten van een voorstelling in onze bioscoop hebben wij gedragsnormen opgesteld. Onderstaande gedragsnormen zullen mede in uw belang gehandhaafd worden in onze bioscoop:
+               Deze bioscoop heeft een ruim aanbod aan versnaperingen die we met zorg voor u geselecteerd hebben en regelmatig vernieuwen. Mocht u aanvullende wensen hebben dan kunt u dit aan ons kenbaar maken in plaats van zelf consumpties mee te brengen.
+               Een rustige en ontspannen sfeer is kenmerkend voor onze bioscoop. Door uw stemvolume aan te passen en uw mobiele telefoon uit te schakelen levert u een belangrijke bijdrage hieraan.
+               Filmproducenten kunnen mede dankzij uw bezoek aan onze bioscoop telkens weer nieuwe films produceren. Door geen opname apparatuur te gebruiken tijdens de voorstelling respecteert u de auteursrechten.
+               We hebben een geavanceerd klimaatbeheersingssysteem in onze bioscoop omdat we veel waarde hechten aan frisse lucht. Roken past daar niet bij en mag daarom alleen buiten de bioscoop.
+               De wetgever heeft bepaald dat wij geen alcohol mogen schenken aan minderjarigen en dat kinderen die jonger zijn dan 16 jaar geen toegang krijgen tot een bioscoopfilm met de leeftijdsgrens 16. Wij handhaven dit in onze bioscoop, klik hier voor meer uitleg.
+               Om ons serviceniveau te kunnen verbeteren, ontvangen wij graag feedback van onze gasten. U kunt hiervoor tijdens de voorstelling de serviceknop gebruiken. Wij staan u graag persoonlijk of telefonisch te woord maar u kunt ook een reactie achterlaten op onze website.
+               Bij NL-talige kinderfilms zijn kinderen van alle leeftijden welkom. Wij adviseren om de leeftijdsclassificatie van kijkwijzer in acht te nemen. Bij NL-talige kinderfilms worden kinderen die nog niet kunnen lopen, gratis toegelaten indien ze plaatsnemen op schoot van een volwassen begeleider. Bij reguliere films worden kinderen die nog niet kunnen lopen niet toegelaten.
+            </p>
+        </div>
+        <div class="card">
+            <h2>Gasten met een beperking</h2>
+            <div class="fakeimg" style="height:200px;">Image</div>
+            <p>Alle twee de zalen zijn toegankelijk voor mindervaliden en de plaatsen zijn zowel telefonisch als online te reserveren. Tevens is er een mindervalidentoilet aanwezig. In de zaal kunnen twee fauteuils verwijderd worden van de achterste rij zodat er plaats ontstaat voor één of twee rolstoelen.
+                Mensen met een visuele beperking kunnen in onze bioscoop gebruik maken van de app EarCatch. Deze app kan op uw eigen smartphone geïnstalleerd worden en biedt audiodescriptie voor bepaalde films.
+                Doven en slechthorenden kunnen gebruik maken van de app SubCatch. Deze app kan op uw eigen smartphone geïnstalleerd worden en beschrijft bij een aantal films wat u niet hoort.
+            </p>
+        </div>
+    </div>
+    <div class="rightcolumn">
+        <div class="card">
+            <h2>Stijlvol</h2>
+            <div class="fakeimg" style="height:100px;">Image</div>
+            <p>De inrichting, die met veel zorg is samengesteld door een gerenomeerde architect, ademt een luxueuse sfeer en straalt een rust uit die past bij een ontspannen avond of middag uit. Gasten nemen in de zaal plaats op ruime fauteuils of zogenaamde "love seats". Dit zijn comfortabele tweezits bankjes zonder armleuning in het midden. Verder is er een tafeltje en de verlichting wordt tijdens de voorstelling gedimd.</p>
+        </div>
+        <div class="card">
+            <h3>Popular Post</h3>
+            <div class="fakeimg"><p>Image</p></div>
+            <div class="fakeimg"><p>Image</p></div>
+            <div class="fakeimg"><p>Image</p></div>
+        </div>
+        <div class="card">
+            <h2>Persoonlijke aandacht</h2>
+            <p>Gasten kunnen rekenen op persoonlijke aandacht. Deze benadering wordt wellicht eerder tijdens een luxe cruise verwacht dan in een bioscoop. Bij aankomst in de zaal wordt men persoonlijk naar de ruime fauteuils begeleid en wordt de uitgebreide menukaart overhandigd. Tijdens de voorstelling kan er met een druk op de serviceknop een hapje en een drankje besteld worden wat vervolgens op de plaats geserveerd worden. Hierdoor zijn de pauzes tijdens de voorstellingen overbodig. Iedere dag is de bioscoop vanaf 11:00 uur telefonisch bereikbaar. In het weekend en in de vakanties zelfs al eerder.</p>
+        </div>
+    </div>
+</div>
+
+<div class="footer">
+    <h2>Footer</h2>
+</div>
+
+
+</body>
+
+</html>
---
+++ includes/navbar.inc.php
@@ -0,0 +1,17 @@
+<?php
+include '../Private/connection.php';
+
+?>
+
+<div class="topnav">
+    <a class="nav-link active" href=index.php?page=>Planning</a>
+    <?php if (isset($_SESSION['userid'])){?>
+        <a href="php/logout.php" style="float:right">Log uit</a>
+    <?php }else {?>
+        <a  href=index.php?page=login style="float:right">Log in</a>
+    <?php }?>
+
+</div>
+
+
+
