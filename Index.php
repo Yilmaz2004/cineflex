fc23ca787875aea89b8e657af21149b4efda3ec3
Ajuub Osman <00303637@rijnijssel.nl>
2022-09-13 08:31:48 +0200

    newest

--- Index.php
+++ Index.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_GET['page'])) {
     $page = $_GET['page'];
 }else{
-    $page='workersview';
+    $page='homepage';
 }
 ?>
 
@@ -17,7 +17,7 @@ if (isset($_GET['page'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 
 
 </head>
